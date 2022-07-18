9e4078cfeeea25b76b8e523c9cacb8c977842f41
Alumno <noemilopezpina@gmail.com>
2022-07-18 20:29:43 +0200

    Commit Inicial seekbar

--- app/src/main/java/com/example/calculadoraimc/MainActivity.kt
+++ app/src/main/java/com/example/calculadoraimc/MainActivity.kt
@@ -2,10 +2,42 @@ package com.example.calculadoraimc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
+import com.example.calculadoraimc.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var b : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        b=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(b.root)
+        b.seekBar.setOnSeekBarChangeListener(object :
+            SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seek: SeekBar, progress: Int, fromUser: Boolean) {
+                b.tvSeekbarAlt.text = seek.progress.toString().plus("/200")
+            }
+
+            override fun onStartTrackingTouch(seek: SeekBar) {
+                //write custom code for progress is starting
+            }
+
+            override fun onStopTrackingTouch(seek: SeekBar) {
+                // write custom code for progress is stopped
+            }
+        })
+        b.seekBar2.setOnSeekBarChangeListener(object :
+            SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seek: SeekBar, progress: Int, fromUser: Boolean) {
+                b.tvSeekbarPeso.text = seek.progress.toString().plus("/150")
+            }
+
+            override fun onStartTrackingTouch(seek: SeekBar) {
+                //write custom code for progress is starting
+            }
+
+            override fun onStopTrackingTouch(seek: SeekBar) {
+                // write custom code for progress is stopped
+            }
+        })
     }
 }
