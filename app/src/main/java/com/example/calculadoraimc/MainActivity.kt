7c9fc5c879f8d31950c00ee222a758a03ed036be
Alumno <noemilopezpina@gmail.com>
2022-07-20 19:43:27 +0200

    Commit custom-dialog hacer en fragment

--- app/src/main/java/com/example/calculadoraimc/MainActivity.kt
+++ app/src/main/java/com/example/calculadoraimc/MainActivity.kt
@@ -1,15 +1,19 @@
 package com.example.calculadoraimc
 
 import android.annotation.SuppressLint
+import android.app.Dialog
 import android.content.Context
+import android.content.DialogInterface
 import android.graphics.Color
 import android.graphics.Color.HSVToColor
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.SeekBar
 import android.widget.Toast
 import androidx.annotation.ColorInt
 import androidx.annotation.ColorRes
+import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat
 import com.example.calculadoraimc.databinding.ActivityMainBinding
 import com.google.android.material.internal.ContextUtils.getActivity
@@ -27,6 +31,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         b=ActivityMainBinding.inflate(layoutInflater)
         setContentView(b.root)
+
+//        AlertDialog.Builder(context!!).setView(R.layout.custom_dialog)
+//            .setPositiveButton(R.string.aceptar,null)
+//            .show()
 
 
         b.seekBar.setOnSeekBarChangeListener(object :
@@ -85,7 +93,10 @@ class MainActivity : AppCompatActivity() {
                             .withColor(getColor(R.color.secondaryColor)).show()
                     in 40.00..MAX_VALUE->
                         Snackbar.make(b.llPrincipal, R.string.morbida, Snackbar.LENGTH_LONG)
-                            .withColor(getColor(R.color.red)).show()
+                            .withColor(getColor(R.color.red)).setAction("Ver Tabla") {
+                                onAlertDialog(b.root)
+                            }
+                            .show()
                 }
 
             }
@@ -101,4 +112,32 @@ class MainActivity : AppCompatActivity() {
         this.view.setBackgroundColor(colorInt)
         return this
     }
+
+    // When User cilcks on dialog button, call this method
+    fun onAlertDialog(view: View) {
+        //Instantiate builder variable
+        val builder = AlertDialog.Builder(view.context)
+
+        builder.setView(R.layout.custom_dialog)
+        builder.setPositiveButton(R.string.aceptar, null)
+        builder.show()
+    }
+//    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+//        return activity?.let {
+//            val builder = AlertDialog.Builder(it)
+//            // Get the layout inflater
+//            val inflater = requireActivity().layoutInflater;
+//
+//            // Inflate and set the layout for the dialog
+//            // Pass null as the parent view because its going in the dialog layout
+//            builder.setView(inflater.inflate(R.layout.custom_dialog, null))
+//                // Add action buttons
+//                .setPositiveButton(R.string.aceptar,
+//                    DialogInterface.OnClickListener { dialog, id ->
+//                        // sign in the user ...
+//                    })
+//
+//            builder.create()
+//        } ?: throw IllegalStateException("Activity cannot be null")
+//    }
 }
